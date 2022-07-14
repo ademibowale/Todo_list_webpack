50cbe2d741dbe7d5d6b1ee3dee26d87ae7435ad9
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-07-14 11:38:14 +0200

    delete add testing

--- addremove.test.js
+++ addremove.test.js
@@ -1,16 +1,5 @@
 import { addItem, todoList } from "./src/modules/code";
-
 test('Add one new item to the list', () => {
-  const e = { stopPropagation: jest.fn() };
-  const todo = {
-    value:'first task'
-  }
-  const todoObj = {
-    description: todo.value,
-    completed: false,
-    index: todoList.length + 1,
-  };
-  todoList.push(todoObj);
-  localStorage.setItem('data', JSON.stringify(todoList));
-  expect(JSON.parse(localStorage.getItem('data')).length).toBe(1);
+ 
+
 });
